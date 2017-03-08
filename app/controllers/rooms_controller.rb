49530574ddc9935b9f334b6e8686470b8f4af3e4
tlockAOB <tlock@armyofbees.com>
2017-03-08 19:39:01 +0700

    milestone 1

---
+++ app/controllers/rooms_controller.rb
@@ -0,0 +1,24 @@
+class RoomsController < ApplicationController
+
+  def create
+    @room = Room.create room_params
+    if @room.save
+      flash[:success] = "Room Created"
+      redirect_to @room
+    else
+      flash[:error] = "Try again"
+      render 'index'
+    end
+  end
+
+  def show
+    @room = Room.find_by(id: params[:id])
+  end
+
+
+  private
+
+  def room_params
+    params.require(:room).permit(:name)
+  end
+end
